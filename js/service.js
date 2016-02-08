7a5c442a632f0a24083a3f1538946b449e86b205
Gábor Tóth <tgabi333@gmail.com>
2016-02-09 00:56:57 +0100

    remove unnecessary declarations fixes for block level variable usages see http://stackoverflow.com/questions/10801747/dealing-with-duplicate-declaration-warning-for-loop-variables fix mistakenly used ES6 object property shorthand (IE11 works) fix for global variable usages removed unused parameters fix various html errors fix jsdocs fix some line endings fix missing semicolons fix some typos remove .git from docker images

--- js/service.js
+++ js/service.js
@@ -32,22 +32,24 @@
 *  Defines services
 */
 angular.module('myModule', []).service('serviceTest', function(){
+    var array, savedGraph, savedData;
 
   // Getter & setter for initialize and retrieve data
 	this.set = function(data){
 		savedData = data;
-	}
+	};
 	this.get = function(){
 		return savedData;
-	}
+	};
+
 
   // Getter & Setter for remediation simulation
   this.setArray = function(id_path, id_remed){
     array = [id_path, id_remed];
-  }
+  };
   this.getArray = function(){
     return array;
-  }
+  };
 
   this.setGraph = function(graph){
     savedGraph = graph;
